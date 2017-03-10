6d587c5c73ccef2523e4f426b07976e667adf641
Jan Tattermusch <jtattermusch@google.com>
2017-03-10 16:15:04 +0100

    initial C# client

---
+++ client/csharp/generate_proto_csharp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+cd $(dirname $0)/../..
+
+PROTOC=$HOME/.nuget/packages/Google.Protobuf.Tools/3.2.0/tools/linux_x64/protoc
+PLUGIN=protoc-gen-grpc=$HOME/.nuget/packages/Grpc.Tools/1.1.0/tools/linux_x64/grpc_csharp_plugin
+$PROTOC exchange_o_gram.proto -I . --plugin=$PLUGIN --csharp_out=client/csharp/ExchangeOGramClient --grpc_out=client/csharp/ExchangeOGramClient 
