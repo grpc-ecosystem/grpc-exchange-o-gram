e80f39916924ade253c458081099f148defcf97b
Jan Tattermusch <jtattermusch@google.com>
2017-03-28 22:12:35 +0200

    add --enable_ssl

--- helpers/grpc_cli_examples.sh
+++ helpers/grpc_cli_examples.sh
@@ -2,6 +2,6 @@
 # Did you set path to your CA?
 # export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH=$HOME/grpc-service-demo/certs/ca.pem
 
-grpc_cli ls -l demo-linux1:8433
+grpc_cli ls -l --enable_ssl demo-linux1:8433
 
-grpc_cli call demo-linux1:8433 exchange_o_gram.WallService.GetWallPosts 'username: "testuser"'
+grpc_cli call --enable_ssl demo-linux1:8433 exchange_o_gram.WallService.GetWallPosts 'username: "testuser"'
